afc8e6ff5c9dd244907a60b030103d4525ca0021
Adam Shufelt <adamshufelt@gmail.com>
2023-03-23 20:08:35 -0500

    Enemies are now affected by gravity, and won't walk off edge of platforms

--- TileVania/Assets/Scripts/EnemyMovement.cs
+++ TileVania/Assets/Scripts/EnemyMovement.cs
@@ -18,14 +18,38 @@ public class EnemyMovement : MonoBehaviour
     {
         myRigidBody.velocity = new Vector2(moveSpeed, 0);
     }
-    /*
-    void OnTriggerExit2D(Collider2D collision)
+
+    void FixedUpdate()
     {
-        Debug.Log("OnTriggerExit2D, enemy" + collision.GetType());
-        moveSpeed = -moveSpeed;
-        FlipEnemyFacing();
+        //RaycastHit2D hit;
+        Vector2 rayOrigin = new Vector2(transform.position.x + ((moveSpeed >= 0) ? 0.4f : -0.4f), transform.position.y);
+        //Vector2 rayOrigin = transform.position;
+        int layerMask = LayerMask.GetMask("Ground");
+        //layerMask = ~layerMask;
+
+        if (Physics2D.Raycast(rayOrigin, Vector2.down, 1f, layerMask))
+        {
+            //Debug.DrawRay(rayOrigin, Vector2.down * 1f, Color.yellow);
+        }
+        else
+        {
+            //Debug.DrawRay(rayOrigin, Vector2.down * 1f, Color.red);
+            //Debug.Log("Not detecting ground");
+            moveSpeed = -moveSpeed;
+            FlipEnemyFacing();
+        }
+        /*
+        if(Physics2D.Raycast(rayOrigin, Vector2.right, 1f))
+        {
+            Debug.DrawRay(rayOrigin, Vector2.right * 1f, Color.yellow);
+        }
+        else
+        {
+            Debug.DrawRay(rayOrigin, Vector2.right * 1f, Color.red);
+        }
+        */
     }
-    */
+    
     void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("OnTriggerExit2D, enemy" + collision.GetType());
